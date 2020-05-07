b1445358fec294e2b44d29324a46ccdc8ff71b7f
Rob Juurlink <rob@juurlink.org>
2020-05-07 18:21:54 +0200

    Editor same style as live site (for title in cover image)

--- themes/Lokaal-Hellendoorn/index.php
+++ themes/Lokaal-Hellendoorn/index.php
@@ -1,104 +1,14 @@
 <?php
-/*
-Template Name: Homepagina
-*/
-?>
-<?php get_header(); ?>
 
-<div class="hero-image home" style="background-image:url('<?php the_field('header_image') ?>')">
-    <div class="content">
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one
+ * of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query,
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ */
 
-        <img src="<?php bloginfo('template_directory'); ?>/images/beeldmerk.png" alt="Lokaal Hellendoorn">
-        <h1>Lokaal Hellendoorn</h1>
-    </div>
-</div>
-<div id="home">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="content-container">
-                    <?php the_field('page_text')?>
-                </div>
-            </div>
-            <div class="col-xs-12">
-                <div class="carousel-title">
-                    <h3>Plaatsen in de gemeente Hellendoorn</h3>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="owl-carousel owl-theme">
-
-    <a href="/daarle">
-        <div class="item daarle">
-            <div class="place-name ">
-                <h4>Daarle</h4>
-            </div>
-        </div>
-    </a>
-
-    <a href="/daarlerveen">
-        <div class="item daarlerveen">
-            <div class="place-name ">
-                <h4>Daarlerveen</h4>
-            </div>
-        </div>
-    </a>
-
-    <a href="/haarle">
-        <div class="item haarle">
-            <div class="place-name ">
-                <h4>Haarle</h4>
-            </div>
-        </div>
-    </a>
-
-    <a href="/hellendoorn">
-        <div class="item hellendoorn">
-            <div class="place-name ">
-                <h4>Hellendoorn</h4>
-            </div>
-        </div>
-    </a>
-
-    <a href="/hulsen">
-        <div class="item hulsen">
-            <div class="place-name ">
-                <h4>Hulsen</h4>
-            </div>
-        </div>
-    </a>
-
-    <a href="/marle">
-        <div class="item marle">
-            <div class="place-name ">
-                <h4>Marle</h4>
-            </div>
-        </div>
-    </a>
-
-    <a href="/nijverdal">
-        <div class="item nijverdal">
-            <div class="place-name ">
-                <h4>Nijverdal</h4>
-            </div>
-        </div>
-    </a>
-    <a href="/egede">
-        <div class="item egede">
-            <div class="place-name ">
-                <h4>Egede,<br>
-                    Hancate,<br>
-                    Eelen <br>
-                    en Rhaan
-                </h4>
-            </div>
-        </div>
-    </a>
-
-</div>
-
-<?php get_footer(); ?>
-
-
+include('_default2020.php');
